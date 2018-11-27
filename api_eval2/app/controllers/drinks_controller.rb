797d05ef9bb7365f95cd20b4d9e71a8692b57169
felipedf <felipe.diogenes13@gmail.com>
2018-11-26 22:46:26 -0300

    init

---
+++ api_eval2/app/controllers/drinks_controller.rb
@@ -0,0 +1,11 @@
+class DrinksController < ApiController
+  def index
+    @drinks = Drink.select('id, title').all
+    render json: @drinks
+  end
+
+  def show
+    @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
+  end
+end
